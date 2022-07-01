8a7ae698f25d79fa4f75cef5ee64438a465999be
Kingc14 <108548088+Kingc14@users.noreply.github.com>
2022-07-01 14:20:28 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+console.log("This is the console");
+var name1="Mahesh ";
+var name2="Chinthapalli";
+var full_place= name1.concat(name2);
+console.log(full_place);
+
+var a="4";
+var b="8"
+var add="12";
+var sub=b-a;
+var times=b*a;
+var div=b/a;
+
+console.log(add)
+console.log(sub)
+console.log(times)
+console.log(div)
